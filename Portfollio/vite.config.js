e9135f9ee9942400d4e6a3a0b5e18ae3a8096efe
sumit <sumitsinghr8424@gmail.com>
2026-01-26 23:15:02 +0530

    Fix Vercel build (disable lightningcss)

--- Portfollio/vite.config.js
+++ Portfollio/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-   base: '/Portfollio/',
-});
+  css: {
+    transformer: 'postcss'
+  }
+})
